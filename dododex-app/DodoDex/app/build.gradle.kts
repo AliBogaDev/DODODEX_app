4ab2319c9627e9ecfbfdbeb6531ac81ad7c4b379
AlinaProject <alibogarin@gmail.com>
2025-07-03 11:45:59 +0200

    Actualizando...

--- dododex-app/DodoDex/app/build.gradle.kts
+++ dododex-app/DodoDex/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.app.dododexapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.dododexapp"
@@ -60,31 +60,34 @@ android {
 
     dependencies {
 
-        implementation("com.google.android.gms:play-services-maps:18.2.0")
-        implementation("androidx.recyclerview:recyclerview:1.3.2")
+        implementation("com.google.android.gms:play-services-maps:19.2.0")
+        implementation("androidx.recyclerview:recyclerview:1.4.0")
         annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
         implementation("com.github.bumptech.glide:glide:4.13.0")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-        implementation("androidx.activity:activity-compose:1.8.2")
-        implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+        implementation("androidx.activity:activity-compose:1.10.1")
+        implementation(platform("androidx.compose:compose-bom:2025.06.01"))
         implementation("androidx.compose.ui:ui")
         implementation("androidx.compose.ui:ui-graphics")
         implementation("androidx.compose.ui:ui-tooling-preview")
         implementation("androidx.compose.material3:material3")
-        androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+        androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.01"))
         androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-        implementation("androidx.appcompat:appcompat:1.6.1")
-        implementation("com.google.android.material:material:1.11.0")
-        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        implementation("androidx.appcompat:appcompat:1.7.1")
+        implementation("com.google.android.material:material:1.12.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.2.1")
         implementation("com.android.volley:volley:1.2.1")
         testImplementation("junit:junit:4.13.2")
-        androidTestImplementation("androidx.test.ext:junit:1.1.5")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        androidTestImplementation("androidx.test.ext:junit:1.2.1")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
         debugImplementation("androidx.compose.ui:ui-tooling")
         debugImplementation("androidx.compose.ui:ui-test-manifest")
-        implementation("androidx.viewpager2:viewpager2:1.0.0")
-        implementation("androidx.navigation:navigation-fragment:2.7.7")
-        implementation("androidx.navigation:navigation-ui:2.7.7")
+        implementation("androidx.viewpager2:viewpager2:1.1.0")
+        implementation("androidx.navigation:navigation-fragment:2.9.1")
+        implementation("androidx.navigation:navigation-ui:2.9.1")
+        implementation ("androidx.lifecycle:lifecycle-runtime-compose-android:2.9.1")
+
+
 
     }
 }
